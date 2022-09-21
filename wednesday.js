f1924e2522af7029f963b7cfbe6f4ec26056e074
Evan Crenshaw <ehc.reader@gmail.com>
2022-09-21 08:43:16 -0600

    publish?

---
+++ wednesday.js
@@ -0,0 +1,7 @@
+function change() {
+  let pagetext = "This is a Div";
+  let newtext = "Hello!";
+
+  document.getElementById("div").innerHTML = newtext;
+  document.getElementById("div").innerHTML = newtext;
+}
