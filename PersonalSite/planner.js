384721afd58c882e9928b2e45ae990e665910e9f
Evan Crenshaw <ehc.reader@gmail.com>
2022-10-28 09:32:15 -0600

    Wordle started

--- PersonalSite/planner.js
+++
@@ -1,7 +0,0 @@
-let word = "new word";
-let number = 2;
-function newassign() {
-  number += 1;
-  document.getElementById("assignments").innerHTML +=
-    '<li id="a' + number + '" >' + word + "</li>";
-}
